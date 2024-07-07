3655fbdd3b96c5bd4c3da41712117b6c6658d75b
Nilesh-geeks <nilesh.20gcebit040@gmail.com>
2024-07-07 23:53:58 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
-
+import Navbar from "@/components/Navbar";
+import Home from "@/components/Home";
+import Clients from "@/components/Clients";
+import Community from "@/components/Community";
+import Footer from "@/components/Footer";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Navbar />
+    <Home />
+    <Clients />
+    <Community />
+    <Footer />
+    <Component {...pageProps} />;
+  </>
 }
